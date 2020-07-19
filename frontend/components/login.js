6c15b326cedc29f34b4196ca4c0e9015b90bdcb4
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-07-19 19:26:11 +0530

    added stack navigator

---
+++ frontend/components/login.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class Login extends Component {
+  render() {
+    return <Text>Login Page</Text>;
+  }
+}
+
+export default Login;
---
+++ frontend/components/signup.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class Signup extends Component {
+  render() {
+    return <Text>Signup Page updated</Text>;
+  }
+}
+
+export default Signup;
